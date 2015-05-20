5f3ce0570b68469b94dac563dd44ea4851c9202c
JoepvdBroek <joep28_12@hotmail.com>
2015-05-20 14:37:37 +0200

    compareTile en isMatchAvailable

--- app/js/GameController.js
+++ app/js/GameController.js
@@ -8,14 +8,44 @@ module.exports = function(app){
 		this.currentGameTiles = [];
 		this.tiles = [];
 
-		GameFactory.getTiles().success(function(data) {
-            console.log(data);
+		var tile1 = {
+	        "xPos": 7,
+	        "yPos": 1,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 76,
+	            "suit": "Tiger",
+	            "name": "2",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bb5"
+	    };
+
+	    var tile2 = {
+	        "xPos": 7,
+	        "yPos": 1,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 76,
+	            "suit": "Tiger",
+	            "name": "3",
+	            "matchesWholeSuit": true,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bb5"
+	    };
+
+		GameFactory.compareTiles(tile1, tile2);
+		GameFactory.isMatchAvailable();
+
+		/*GameFactory.getTiles().success(function(data) {
             this.tiles = data;
 
         }).error(function(status, data) {
             console.log(status);
             console.log(data);
-        });
+        });*/
 
 		this.addGame = function() {
 			GameFactory.addGame(this.newGame);
--- app/js/GameFactory.js
+++ app/js/GameFactory.js
@@ -53,6 +53,190 @@ module.exports = function(app){
 			}
 		}
 
+		factory.compareTiles = function(tile1, tile2){
+
+			if(tile1.tile.suit == tile2.tile.suit){
+				//als 1 van de 2 tiles false is moet name ook overeenkomen
+				if(tile1.tile.matchesWholeSuit == false || tile2.tile.matchesWholeSuit == false){
+					if(tile1.tile.name == tile2.tile.name){
+						console.log('match!');
+					} else {
+						console.log('no match');
+					}
+				} else {
+					console.log('match!');
+				}	
+			} else {
+				console.log('no match');
+			}
+		}
+
+		function compareTiles(tile1, tile2){
+			if(tile1.tile.suit == tile2.tile.suit){
+				//als 1 van de 2 tiles false is moet name ook overeenkomen
+				if(tile1.tile.matchesWholeSuit == false || tile2.tile.matchesWholeSuit == false){
+					if(tile1.tile.name == tile2.tile.name){
+						return true;
+					} else {
+						return false;
+					}
+				} else {
+					return true;
+				}	
+			} else {
+				return false;
+			}
+		}
+
+		var collection = [
+	    {
+	        "xPos": 7,
+	        "yPos": 1,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 76,
+	            "suit": "Character",
+	            "name": "2",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bb5"
+	    },
+	    {
+	        "xPos": 9,
+	        "yPos": 1,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 135,
+	            "suit": "Dragon",
+	            "name": "White",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bb6"
+	    },
+	    {
+	        "xPos": 11,
+	        "yPos": 2,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 10,
+	            "suit": "Circle",
+	            "name": "3",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bb7"
+	    },
+	    {
+	        "xPos": 13,
+	        "yPos": 2,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 37,
+	            "suit": "Bamboo",
+	            "name": "1",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bb8"
+	    },
+	    {
+	        "xPos": 22,
+	        "yPos": 2,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 102,
+	            "suit": "Character",
+	            "name": "8",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bb9"
+	    },
+	    {
+	        "xPos": 5,
+	        "yPos": 3,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 29,
+	            "suit": "Circle",
+	            "name": "8",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bba"
+	    },
+	    {
+	        "xPos": 7,
+	        "yPos": 3,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 59,
+	            "suit": "Bamboo",
+	            "name": "6",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bbb"
+	    },
+	    {
+	        "xPos": 9,
+	        "yPos": 3,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 143,
+	            "suit": "Flower",
+	            "name": "Bamboo",
+	            "matchesWholeSuit": true,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bbc"
+	    },
+	    {
+	        "xPos": 27,
+	        "yPos": 3,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 20,
+	            "suit": "Circle",
+	            "name": "6",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bbd"
+	    },
+	    {
+	        "xPos": 27,
+	        "yPos": 3,
+	        "zPos": 0,
+	        "tile": {
+	            "_id": 20,
+	            "suit": "Circle",
+	            "name": "6",
+	            "matchesWholeSuit": false,
+	            "__v": 0
+	        },
+	        "_id": "5541fc5b1872631100678bbg"
+	    }];
+
+		factory.isMatchAvailable = function(){
+			collection.forEach(function(tile1) {
+				collection.forEach(function(tile2) {
+					if(tile2._id == tile1._id){
+						//zelfde tile
+					} else {
+						if(compareTiles(tile1, tile2)){
+							console.log('match found!');
+							return true;
+						}
+					}
+				});
+			});
+			console.log('no match found');
+			return false;
+		}
+
 		return factory;
 	}]);
 }
